9cd130a6db282d9ad9f712404309d318a78fba0f
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-10 04:33:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,15 @@
 class Dog
  
-  def name=(dogs_name)
-    @name = dogs_name
+  def name=(persons_name)
+    @name = persons_name
   end
  
   def name
     @name
   end
   
-  def breed=(dogs_breed)
-    @breed = dogs_breed
+  def breed=(persons_name_breed)
+    @breed = persons_name 
   end
  
   def breed 
