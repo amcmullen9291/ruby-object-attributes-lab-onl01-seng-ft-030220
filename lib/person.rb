5abef61403ce324dceba1a292488126d9ac1d18d
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-10 04:21:33 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+ 
+  def name=(person_name)
+    @this_persons_name = person_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+  
+  def job=(person_breed)
+    @this_persons_job = person_job
+  end
+ 
+  def job
+    @this_persons_job
+  end
+end
